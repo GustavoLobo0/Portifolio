2185d07eeed957f997b9e151fd9b90a34bb06aaf
gusta1235678 <gustavocesarlobo@gmail.com>
2024-07-18 13:49:10 -0300

    page initial

---
+++ src/Componentes/Header/index.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+export function Header() {
+    return (
+        <header>
+            <h1>Olá, Seja Bem vindo!</h1>
+                <div className="retangulo"></div>
+                <div className="retangulo2"></div>
+        </header>
+    );
+}
---
+++ src/Componentes/PrimeiroConteudo/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+export function PrimeiroConteudo() {
+    return (
+        <>
+            <div className="fundo">
+                <div className="fundoQuadrado1"></div>
+                <div className="fundoQuadrado2"></div>
+                <div className="fundoQuadrado3"></div>
+            </div>
+        </>
+    )
+} 
