ebaf7c06841272e5fa0ed7b1d40484d54c97a1f0
GustavoLobo0 <gustavocesarlobo@gmail.com>
2024-07-22 22:18:04 -0300

    new designer

--- src/Componentes/PrimeiroConteudo/index.js
+++ src/Componentes/PrimeiroConteudo/index.js
@@ -4,14 +4,14 @@ export function PrimeiroConteudo() {
     return (
         <>
             <div className="fundo">
-                <div className="fundoQuadrado1"></div>
-                <div className="fundoQuadrado2"></div>
-                <div className="fundoQuadrado3"></div>
-            </div>
-            <div className="linha">
+                <div className="fundoQuadrado"></div>
                 <div className="linha1"></div>
                 <div className="linha2"></div>
                 <div className="linha3"></div>
+            </div>
+
+            <div className="linha">
+
             </div>
         </>
     )
